31fbc15aaf5a44a46a8c38582ac59cf8e96a11ef
drewlair <drewl@dhcp-10-12-168-222.dhcp.nd.edu>
2023-04-01 15:57:12 -0400

    carousel update

--- hackerz/src/App.js
+++ hackerz/src/App.js
@@ -1,15 +1,34 @@
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from 'react-router-dom';
+
+
+
+
 import './App.css';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact'
+import Navbar from './Navbar'
 
 function App() {
 
 
 
 
-
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+   
+      <div className="App">
+        <Navbar />
+        <Home />
+       
+      </div>
+
+    
+    
   );
 }
 
---
+++ hackerz/src/Navbar.js
@@ -0,0 +1,11 @@
+
+
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <h1 className="logo">varisong</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
